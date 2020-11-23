44845ebe379e3705e930350bfad19df528084fd3
Roman <roman82direct@yandex.ru>
2020-11-23 23:30:24 +0300

    gallery on index

--- controllers/C_Index.php
+++ controllers/C_Index.php
@@ -8,6 +8,8 @@ class C_Index extends C_Base
 	
 	public function action_index(){
 		$this->title .= ' Главная';
-		$this->render('index.html', ['title' => $this->title, 'main' => '1']);	
+
+        $goods = db::getRows('SELECT * FROM goods limit 4', []);
+		$this->render('index.html', ['title' => $this->title, 'main' => '1', 'goods' => $goods]);
 	}
 }
