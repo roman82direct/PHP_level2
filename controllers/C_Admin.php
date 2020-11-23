868e836b5b1d444bedae3cefd363d6e740bafc56
Roman <roman82direct@yandex.ru>
2020-11-23 23:18:49 +0300

    admin

--- controllers/C_Admin.php
+++ controllers/C_Admin.php
@@ -3,8 +3,18 @@
 class C_Admin extends C_Base
 {
 
-    public function action_index(){
+    public function action_index()
+    {
         $this->title .= ' Админка';
-        $this->render('admin.html', ['title' => $this->title, 'admin' => '1']);
+
+        $userId = $_SESSION['user']['id'];
+        $admin = db::getRow('SELECT * FROM user_role where id_user = :userId', ['userId' => $userId]);
+
+        $this->render('admin.html', ['title' => $this->title,  'admin' => $admin['id_role'], 'name' => $_SESSION['user']['login']]);
+    }
+
+    public function action_view()
+    {
+        $this->action_index();
     }
 }
