3c50f6a28a6133e75b1bece12d8bc8da89fa13a7
Roman <roman82direct@yandex.ru>
2020-12-01 12:20:52 +0300

    savegoog processing

--- controllers/C_Admin.php
+++ controllers/C_Admin.php
@@ -1,20 +1,43 @@
 <?php
+require_once('../models/M_Admin.php');
 
-class C_Admin extends C_Base
-{
+class C_Admin extends C_Base {
 
-    public function action_index()
-    {
+    public function action_index(){
         $this->title .= ' Админка';
-
-//        $userRole = $_SESSION['user']['role'];
-//        $admin = db::getRow('SELECT * FROM user_role where id_user = :userId', ['userId' => $userId]);
-
         $this->render('admin.html', ['title' => $this->title,  'admin' => $_SESSION['user']['role'], 'name' => $_SESSION['user']['name']]);
     }
 
-    public function action_view()
-    {
+    public function action_view(){
         $this->action_index();
+    }
+
+    public function action_renderGoods(){
+
+    }
+
+    public function action_renderOrders(){
+
+    }
+
+    public function action_changeImg(){
+
+    }
+
+    public function action_saveGood(){
+        echo $_POST['goodId'];
+        if ($this -> IsPost()){
+            $admin = new M_Admin($_POST['goodId'], $_POST['title'], $_POST['price'], $_POST['discr']);
+            $admin->saveGood();
+        }
+        $this->action_index();
+    }
+
+    public function action_deleteGood(){
+
+    }
+
+    public function action_loadnewGood(){
+
     }
 }
